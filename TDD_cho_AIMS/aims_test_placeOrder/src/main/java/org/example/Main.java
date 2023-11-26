fd9082c20aceef2e584fcfb4f1cc416301db6fe6
HaiDinh2020 <haidinh6008@gmail.com>
2023-11-27 01:12:29 +0700

    payment vnpay

--- TDD_cho_AIMS/aims_test_placeOrder/src/main/java/org/example/Main.java
+++ TDD_cho_AIMS/aims_test_placeOrder/src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 import javax.servlet.annotation.WebServlet;
 import java.io.BufferedReader;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@ComponentScan()
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
--- TDD_cho_AIMS/aims_test_placeOrder/src/main/java/utils/Configs.java
+++ TDD_cho_AIMS/aims_test_placeOrder/src/main/java/utils/Configs.java
@@ -56,8 +56,8 @@ public class Configs {
 
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_ReturnUrl = "http://localhost:8080/vnpay_jsp/vnpay_return.jsp";
-    public static String vnp_TmnCode = "";
-    public static String secretKey = "";
+    public static String vnp_TmnCode = "UTCPMPYJ";
+    public static String secretKey = "DZYNJSCVZPXFXETMMHNAECYSGXSRJZQS";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
